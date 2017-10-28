d22a8129a80d054a416dad1a1c97e3b5f5fb8f23
Yulong Liang <yulong.liang@utah.edu>
2017-10-27 23:00:00 -0600

    Finished all

--- hw6/public/js/shiftChart.js
+++ hw6/public/js/shiftChart.js
@@ -19,6 +19,7 @@ class ShiftChart {
         this.years = [1940, 1944, 1948, 1952, 1956, 1960, 1964, 1968, 1972, 1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012];
     };
 
+
     /**
      * Creates a list of states that have been selected by brushing over the Electoral Vote Chart
      *
@@ -33,6 +34,8 @@ class ShiftChart {
 
         let states = this.states;
         let years = this.years;
+        // let highlight = this.highlight;
+        // let unhighlight = this.unhighlight;
 
         // ******* TODO: PART V *******
 
@@ -73,17 +76,11 @@ class ShiftChart {
             .range([d3.rgb("#007AFF"), d3.rgb('#FFF500')])
 
 
-        //Domain definition for global color scale
         let domain = [-100, 0, 100];
-
-        //Color range for global color scale
         let range = ["#063e78", "#08519c", "#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#fcbba1", "#fc9272", "#fb6a4a", "#de2d26", "#a50f15", "#860308"];
-
-        //ColorScale be used consistently by all the charts
         let colorScale2 = d3.scaleQuantile()
             .domain(domain)
             .range(range);
-
 
         let xAxis = d3.axisBottom()
             .scale(xScale)
@@ -118,6 +115,10 @@ class ShiftChart {
                 return colorScale2(i);
             })
 
+        let tooltip = d3.select("#shiftChart").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
         let label = this.svg.append("g");
         label.append("text")
             .text("Republican")
@@ -132,11 +133,12 @@ class ShiftChart {
             .attr("fill", "#3182bd")
             .style("font-size", "26px")
 
-
         let offset = width / (years.length * 2)
         let dotgroup = this.svg.append("g")
+            .attr("id", "dotgroup")
             .attr("transform", "translate("+(30 + offset)+", 10)");
         let linegroup = this.svg.append("g")
+            .attr("id", "linegroup")
             .attr("transform", "translate("+(30 + offset)+", 10)");
 
         for (let j = 0; j < years.length; j++) {
@@ -149,9 +151,22 @@ class ShiftChart {
                             dotgroup.append("circle")
                                 .attr("cx", x)
                                 .attr("cy", y)
-                                .attr("r", 4)
+                                .attr("r", 10)
                                 .attr("fill", colorScale(i))
                                 .attr("id", states[i])
+                                .on("mouseover", function () {
+                                    let name = d3.select(this).attr("id");
+                                    highlight(name);
+                                    tooltip.html(name)
+                                        .style("left", d3.event.pageX+"px")
+                                        .style("top", (d3.event.pageY + 50)+"px")
+                                        .style("opacity", 1)
+                                })
+                                .on("mouseout", function () {
+                                    unhighlight(d3.select(this).attr("id"));
+                                    tooltip.style("opacity", 0);
+                                })
+
                             if (j != 0) {
                                 let x2 = xScale(parseInt(years[j]-4));
                                 let y2 = yScale(parseFloat(stateData["Last_RD_Difference"]));
@@ -161,7 +176,21 @@ class ShiftChart {
                                     .attr("y1", y)
                                     .attr("y2", y2)
                                     .attr("stroke", colorScale(i))
+                                    .attr("stroke-width", "5px")
                                     .attr("id", states[i])
+                                    .on("mouseover", function () {
+                                        let name = d3.select(this).attr("id");
+                                        highlight(name);
+                                        tooltip.html(name)
+                                            .style("left", d3.event.pageX+"px")
+                                            .style("top", d3.event.pageY+"px")
+                                            .style("opacity", 1)
+                                    })
+                                    .on("mouseout", function () {
+                                        unhighlight(d3.select(this).attr("id"));
+                                        tooltip.style("opacity", 0);
+
+                                    })
                             }
                         }
                     }
@@ -169,28 +198,18 @@ class ShiftChart {
             })
         }
 
+        function highlight (name) {
+            d3.select("#dotgroup").selectAll("#"+name)
+                .attr("r", "15px")
+            d3.select("#linegroup").selectAll("#"+name)
+                .attr("stroke-width", "10px")
+        };
 
-        // console.log(data)
-        // console.log(data.values())
-        // let value = Array.from(data.values())
-        // console.log(value)
-        // let test = value.next();
-        // console.log(test)
-        // let cao = test['0']
-        // console.log(cao)
-        // let linegroup = this.svg.append("g");
-        //
-        // let value = Array.from(data.values());
-        // console.log(value)
-        // let lineGenerator = d3.line()
-        // // .x((d, i) => xScale(d[i].year))
-        // // .y((d, i) => yScale(d[i].data))
-        // let pathString = lineGenerator(value)
-        // console.log(pathString)
-        // let line = linegroup.append("path")
-        // // .transition()
-        // // .duration(1000)
-        //     .attr("d", pathString);
-
+        function unhighlight (name) {
+            d3.select("#dotgroup").selectAll("circle")
+                .attr("r", "10px")
+            d3.select("#linegroup").selectAll("line")
+                .attr("stroke-width", "5px")
+        };
     };
 }
